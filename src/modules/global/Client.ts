f9eb33f37755089b6a60305a7b294fafefb500d9
kjh9211 <aarkim3222@gmail.com>
2026-09-18 20:19:48 +0900

    클라이언트 옵션에 Id 및 비밀번호 속성 추가

--- src/modules/global/Client.ts
+++ src/modules/global/Client.ts
@@ -1,7 +1,10 @@
+import axios from "axios";
 import { EventMap, Events } from "./Events.ts";
 
 interface ClientOptions {
   baseUrl: string;
+  Id?: string;
+  password?: string;
 }
 
 type Listener<T extends unknown[]> = (...args: T) => void;
@@ -9,10 +12,33 @@ type Listener<T extends unknown[]> = (...args: T) => void;
 export class Client {
   private readonly baseUrl: string;
 
+  private readonly Id?: string;
+
+  private readonly password?: string;
+
   private readonly listeners = new Map<Events, Set<(...args: any[]) => void>>();
 
   constructor(options: ClientOptions) {
     this.baseUrl = options.baseUrl;
+    if (options.Id) {
+      this.Id = options.Id;
+    }
+    if (options.password) {
+      this.password = options.password;
+    }
+    this.initialize();
+  }
+
+  protected initialize(): void {}
+
+  public async login(): Promise<boolean> {
+    try {
+      await axios.post(`${this.baseUrl}/auth/signin`, {
+        Id: this.Id,
+        password: this.password,
+      });
+    } catch (error) {}
+    return Promise.resolve(true);
   }
 
   on<K extends Events>(event: K, callback: Listener<EventMap[K]>): this {
